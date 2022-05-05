9898299d7bbf278124d637f25a95702920eb7970
qjoshlawlor <joshlawlor1@gmail.com>
2022-05-05 00:26:16 -0400

    Put band-aid on keyboard bug. Added riddles, and ability to change my words easier

--- app.js
+++ app.js
@@ -7,6 +7,26 @@
 //Need win condition
 
 
+//**QUESTION BANK**/
+class Question {
+    constructor(riddle){
+        this.riddle = riddle;
+    }
+}
+const question1 = new Question(
+    `What is the name of the AI in 2001 A Space Odyessey?`
+);
+const question2 = new Question
+(`What is Always Old, Sometimes New.  
+Never Sad, Sometimes Blue.   
+Never Empty, Sometimes Full.  
+Never Pushes, Always Pulls.`);
+
+const question3 = new Question(
+    `I have never been respected, but I am named after a Greek God! What planet am I?`
+)
+
+
 //WORD BANK
 class WordBank {
     constructor(letters = []){
@@ -14,9 +34,9 @@ class WordBank {
     }
 }
 
-const earth = new WordBank(["E", "A", "R", "T", "H"])
-const moon = new WordBank(["M", "O" , "O", "N"])
-const pluto = new WordBank(["P", "L", "U", "T", "O"])
+const word1 = new WordBank(["H","A","L"])
+const word2 = new WordBank(["M", "O" , "O", "N"])
+const word3 = new WordBank(["P", "L", "U", "T", "O"])
 
 
 let playerScore = 0;
@@ -30,7 +50,7 @@ let boxes = [ ];
 function makeBoxes() {
     
     if(currentRound === 1){
-        for(let i = 0; i < earth.letters.length; i++){
+        for(let i = 0; i < word1.letters.length; i++){
             let box = document.createElement("div")
             box.classList.add("box");
             box.setAttribute("id", i + 1)
@@ -38,7 +58,7 @@ function makeBoxes() {
             boxes.push(box) 
         }
     }else if(currentRound === 2){
-        for(let i = 0; i < moon.letters.length; i++){
+        for(let i = 0; i < word2.letters.length; i++){
             let box = document.createElement("div")
             box.classList.add("box");
             box.setAttribute("id", i + 1)
@@ -46,7 +66,7 @@ function makeBoxes() {
             boxes.push(box)
         }
     }else if(currentRound === 3){
-        for(let i = 0; i < pluto.letters.length; i++){
+        for(let i = 0; i < word3.letters.length; i++){
             let box = document.createElement("div")
             box.classList.add("box");
             box.setAttribute("id", i + 1)
@@ -93,121 +113,108 @@ function startGame (){
     let start = document.getElementById("startButton")
     start.style.display = "none";
     countDown();
+    let question = document.querySelector("#gameText")
+    question.innerHTML = question1.riddle;
 }
 
-//**RESET BUTTON**//
-let reset = document.getElementById("resetButton")
-reset.addEventListener("click", (e)=> {
-    return window.location.reload();
-})
 
 
-
-//This function takes the value of user's click, and compares it to each letter 
-// in our word bank
-// Need to add win function, if all letterboxes are full, user wins
+//**GUESSING FUNCTION**/
 function guessWord(letter) { 
 
     if(currentRound === 1){
-        if (letter === earth.letters[0] && boxes[0].innerHTML !== earth.letters[0]){
+        if (letter === word1.letters[0] && boxes[0].innerHTML !== word1.letters[0]){
             playerScore = playerScore + 1;
             boxes[0].innerHTML = letter;
             guessedWord.push(letter);
             return nextRound();
-        }else if(letter === earth.letters[1] && boxes[1].innerHTML !== earth.letters[1]){
+        }else if(letter === word1.letters[1] && boxes[1].innerHTML !== word1.letters[1]){
             playerScore = playerScore + 1;
             boxes[1].innerHTML = letter;
             guessedWord.push(letter);
             return nextRound();
-        }else if(letter === earth.letters[2] && boxes[2].innerHTML !== earth.letters[2]){
+        }else if(letter === word1.letters[2] && boxes[2].innerHTML !== word1.letters[2]){
             playerScore = playerScore + 1;
             boxes[2].innerHTML = letter;
             guessedWord.push(letter);
             return nextRound();
-        }else if(letter === earth.letters[3] && boxes[3].innerHTML !== earth.letters[3]){
+        }else if(letter === word1.letters[3] && boxes[3].innerHTML !== word1.letters[3]){
             playerScore = playerScore + 1;
             boxes[3].innerHTML = letter;
             guessedWord.push(letter);
             return nextRound();
-        }else if(letter === earth.letters[4] && boxes[4].innerHTML !== earth.letters[4]){
+        }else if(letter === word1.letters[4] && boxes[4].innerHTML !== word1.letters[4]){
             playerScore = playerScore + 1;
             boxes[4].innerHTML = letter;
             guessedWord.push(letter);
             return nextRound();
-        }else {
-            let removeLetter = document.querySelector(`[data-key="${letter}"]`)
-            removeLetter.remove()
         }
+        // else {
+        //     let removeLetter = document.querySelector(`[data-key="${letter}"]`)
+        //     removeLetter.remove()
+        // }
     }else if(currentRound === 2){
-        if (letter === moon.letters[0] && boxes[0].innerHTML !== moon.letters[0]){
+        if (letter === word2.letters[0] && boxes[0].innerHTML !== word2.letters[0]){
             playerScore = playerScore + 1;
             boxes[0].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === moon.letters[1] && boxes[1].innerHTML !== moon.letters[1]){
+        }else if(letter === word2.letters[1] && boxes[1].innerHTML !== word2.letters[1]){
             playerScore = playerScore + 1;
             boxes[1].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === moon.letters[2] && boxes[2].innerHTML !== moon.letters[2]){
+        }else if(letter === word2.letters[2] && boxes[2].innerHTML !== word2.letters[2]){
             playerScore = playerScore + 1;
             boxes[2].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === moon.letters[3] && boxes[3].innerHTML !== moon.letters[3]){
+        }else if(letter === word2.letters[3] && boxes[3].innerHTML !== word2.letters[3]){
             playerScore = playerScore + 1;
             boxes[3].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === moon.letters[4] && boxes[4].innerHTML !== moon.letters[4]){
+        }else if(letter === word2.letters[4] && boxes[4].innerHTML !== word2.letters[4]){
             playerScore = playerScore + 1;
             boxes[4].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else {
-            let removeLetter = document.querySelector(`[data-key="${letter}"]`)
-            removeLetter.remove()
         }
+        // else {
+        //     let removeLetter = document.querySelector(`[data-key="${letter}"]`)
+        //     removeLetter.remove()
+        // }
     }else if(currentRound === 3){
-        if (letter === pluto.letters[0] && boxes[0].innerHTML !== pluto.letters[0]){
+        if (letter === word3.letters[0] && boxes[0].innerHTML !== word3.letters[0]){
             playerScore = playerScore + 1;
             boxes[0].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === pluto.letters[1] && boxes[1].innerHTML !== pluto.letters[1]){
+        }else if(letter === word3.letters[1] && boxes[1].innerHTML !== word3.letters[1]){
             playerScore = playerScore + 1;
             boxes[1].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === pluto.letters[2] && boxes[2].innerHTML !== pluto.letters[2]){
+        }else if(letter === word3.letters[2] && boxes[2].innerHTML !== word3.letters[2]){
             playerScore = playerScore + 1;
             boxes[2].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === pluto.letters[3] && boxes[3].innerHTML !== pluto.letters[3]){
+        }else if(letter === word3.letters[3] && boxes[3].innerHTML !== word3.letters[3]){
             playerScore = playerScore + 1;
             boxes[3].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else if(letter === pluto.letters[4] && boxes[4].innerHTML !== pluto.letters[4]){
+        }else if(letter === word3.letters[4] && boxes[4].innerHTML !== word3.letters[4]){
             playerScore = playerScore + 1;
             boxes[4].innerHTML = letter;
             guessedWord.push(letter);
-            console.log(guessedWord)
             return nextRound();
-        }else {
-            let removeLetter = document.querySelector(`[data-key="${letter}"]`)           
-            removeLetter.remove()
         }
+        // else {
+        //     let removeLetter = document.querySelector(`[data-key="${letter}"]`)           
+        //     removeLetter.remove()
+        // }
     }    
     
 }
@@ -216,17 +223,22 @@ function guessWord(letter) {
 //**NEXT ROUND FUNCTION**/
 function nextRound(){
     guessedWord.sort();
-    if(guessedWord.join('') === "AEHRT"){
+    if(guessedWord.join('') === "AHL"){
         currentRound += 1;
+        let question = document.querySelector("#gameText")
+        question.innerHTML = question2.riddle;
+
         let oldBoxes = document.querySelectorAll(".box")
         oldBoxes.forEach(function (e){
             e.remove();
         })
-        boxes = [] //THIS IS THE FIX TO MY BUG!
+        boxes = [] 
         makeBoxes();
         guessedWord = []
     }else if(guessedWord.join('') === "MNOO"){
         currentRound += 1;
+        let question = document.querySelector("#gameText")
+        question.innerHTML = question3.riddle;
         let oldBoxes = document.querySelectorAll(".box")
         oldBoxes.forEach(function (e){
             e.remove();
@@ -271,16 +283,16 @@ function gameOver(){
         timerTitle.style.fontSize = "100px";
         timerTitle.style.color = "red";
         timeLeft.remove()
-    
+
+        let keys = document.querySelector("#keyBoardRows")
+        keys.remove()
     }
 }
 
-
-//*WIN/LOSE ANIMATIONS*/
-function winAnimation() {
-    
-}
-
-
+//**RESET BUTTON**//
+let reset = document.getElementById("resetButton")
+reset.addEventListener("click", (e)=> {
+    return window.location.reload();
+})
 
 
